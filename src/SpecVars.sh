f21b6ca2f4e136295205057f342e4afad0c3c807
Ivette <ivettevindas@hotmail.com>
2016-08-02 12:44:35 -0600

    Scripts

--- src/SpecVars.sh
+++ src/SpecVars.sh
@@ -2,26 +2,19 @@
 
 ## Este script revisa los argumentos de un script
 
-# Definicion de variables
+#  Definicion de variables
 ERRORLOG="$0.err"
 
-#Ejemplo1
-#if cp archivo1.txt archivo1.txt.bkup 2> $0.err
-#then
-#	echo "el comando se ejecuto correctamente"
-#fi
 
-
-#Ejemplo2
 if cp archivo1.txt archivo1.txt.bkup 2> $ERRORLOG
 then
 	if cp archivo2.txt archivo2.txt.bkup 2> $ERRORLOG
 	then
- 		echo "el respaldo de ambos archivos se ejecuto correctamente"
+		echo "el respaldo de ambos archivos se ejecuto correctamente"
 	fi
 else
-	echo "El archivo1 no pudo ser respaldado, por lo tanto el archivo2 
-tampoco fue respaldado, verifique $ERRORLOG"
-	cat $ERRORLOG | mail -s "error de respaldo" ivettevindas@hotmail.com
+	echo "El archivo1 no pudo ser respaldado, por lo tanto el archivos 
+dos tampoco fue respaldado, verifique $ERRORLOG"
+	#cat $ERRORLOG | mail -s "Error de respaldo" uk3xd8@gmail.com
+	
 fi
-
